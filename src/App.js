a4060326ddba5a2a8d2e1f15bf5c2989a4720dc2
William Vitorino <williamvitorino3@gmail.com>
2020-03-30 17:36:56 -0300

    Onganização princilpal do sistema.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './App.scss'
 import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
   return (
     <section>
       <Header />
+      <Home />
     </section>
   )
 }
